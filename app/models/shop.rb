c33a297a9baffbff93ed2c3a7b8b1654ac6185d3
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2019-02-23 01:47:51 +0700

    Add serailize method to shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,14 @@
 class Shop < ApplicationRecord
+	belongs_to :category
+	
 	reverse_geocoded_by :latitude, :longitude
+
+	def serialize
+		{
+			name: name,
+			latitude: latitude,
+			longitude: longitude,
+			icon: category.marker_url
+		}
+	end
 end
