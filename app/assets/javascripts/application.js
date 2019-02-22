adea3e7c62ae5923c6fa792576bce65b050ebc5a
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2019-02-23 01:49:00 +0700

    Move javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+const bangkokLocation = {lat: 13.736717, lng: 100.523186}
+
+function searchShop(map, lat, lng) {
+  axios.get("map/search", {params: {latitude: lat, longitude: lng}}).then(resolve => {
+  	let shops = resolve.data.data
+  	shops.map(shop => {
+  		let marker = new google.maps.Marker({
+  			position: {lat: shop.latitude, lng: shop.longitude},
+  			map: map,
+  			icon: {url: shop.icon}
+  		});
+
+  		let infowindow = new google.maps.InfoWindow({
+         content: shop.name
+      });
+
+			marker.addListener('click', function() {
+				infowindow.open(map, marker);
+			});  		
+  	})
+  })
+}
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: bangkokLocation.lat, lng: bangkokLocation.lng},
+    zoom: 14
+  });
+
+	map.addListener('dragend', function() {
+		let lat = this.center.lat();
+		let lng = this.center.lng();
+
+		searchShop(map, lat, lng)
+  });
+
+	searchShop(map, bangkokLocation.lat, bangkokLocation.lng)
+}
