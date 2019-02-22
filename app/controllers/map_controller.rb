f45d88fa6ffbeb02e25f826a710116da7fdfa10b
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2019-02-23 01:48:14 +0700

    Add seach to map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,11 @@
 class MapController < ApplicationController
   def index
   end
+
+  def search
+  	shops = Shop.includes(:category).near([params[:latitude], params[:longitude]], 3, units: :km)
+  	data = shops.map(&:serialize)
+
+  	render json: {data: data}
+  end
 end
